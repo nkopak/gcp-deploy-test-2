36754416e044f139a7aaf8eb44348f37641f71b2
nkopak <123nazik@gmail.com>
2021-11-05 13:03:59 +0200

    changes 2

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-"use strict"
-
-// [START gae_node_request_example]
 const express = require("express")
 
 const app = express()
 
 app.get("/", (req, res) => {
-  res.status(200).send("Deploy changes test").end()
+  // res.status(200).send("Deploy changes test").end()
+  res.send("Breadchyk is a great guy")
 })
 
 // Start the server
-const PORT = process.env.PORT || 8080
+const PORT = 8080
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
-  console.log("Press Ctrl+C to quit.")
 })
-// [END gae_node_request_example]
 
 module.exports = app
